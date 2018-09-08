c78e1e73a81d4353880362cf800d1cea163fdad9
Will Garrett <will.garrett2k@gmail.com>
2018-09-08 02:54:39 -0700

    Neo4j connection test completed passing

---
+++ tests/Feature/Neo4jTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use GraphAware\Neo4j\Client\ClientBuilder;
+class Neo4j extends TestCase
+{
+
+    public function testNeo4JClient(){
+        $pass = urlencode("tango461");
+        $client = ClientBuilder::create()
+        ->addConnection('default', 'http://neo4j:'.$pass.'@neo4j:7474')
+        ->build();
+        $this->assertInstanceOf('GraphAware\Neo4j\Client\Client', $client);
+        //$create = $client->run('CREATE (n:Person) SET n += {infos}', ['infos' => ['name' => 'Will', 'age' => 35]]);
+        $result = $client->run('MATCH (n:Person{name:"Will"}) RETURN n');
+        $items = $result->firstRecord();
+        $record=$items->value("n")->values();
+        $this->assertArraySubset(['name'=>'Will', 'age'=>35], $record);
+        
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
