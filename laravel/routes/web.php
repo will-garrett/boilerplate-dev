edccadb9cee5006ff0de0aabca0a523a4d94771c
Will Garrett <will.garrett2k@gmail.com>
2018-11-14 01:23:09 -0800

    TODO: configure xdebug with local environment
    added phpinfo route to laravel for configuration viewing

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/sysinfo', function(){
+Route::get('/phpinfo', function(){
     return view('phpinfo');
 });
